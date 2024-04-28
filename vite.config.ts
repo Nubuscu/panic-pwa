788cd24aae5aaf821a49a84e830d97396df38781
Nubuscu <zippyjr1@gmail.com>
2024-04-28 22:26:25 +1200

    Make app installable as PWA
    very low-effort. there's no remote content to fetch, so no real need to
    check for updates unless I make changes to the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-
+import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Panic At The Dojo Character Sheet PWA",
+        short_name: "PatD PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     open: true,
   },
