a665884e657df6ee5a07873fa76210a7b8550d07
Nubuscu <zippyjr1@gmail.com>
2024-04-29 23:11:51 +1200

    Add gh-pages deploy scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://nubuscu.github.io/panic-pwa/",
   plugins: [
     react(),
     VitePWA({
