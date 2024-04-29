9cb0d56344ec5db0c9e2ed670a490753fc5662af
Nubuscu <zippyjr1@gmail.com>
2024-04-29 22:57:38 +1200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://nubuscu.github.io/panic-pwa/sw.js', { scope: 'https://nubuscu.github.io/panic-pwa/' })})}
