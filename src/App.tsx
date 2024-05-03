befd4517621939fceecee7901f0cd248c5354dc2
Tyler Kennedy <tyler.kennedy@seequent.com>
2024-05-03 14:07:15 +1200

    Add hero name title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Container, Grid, StyledEngineProvider } from "@mui/material"
+import { Container, Grid, StyledEngineProvider, Typography } from "@mui/material"
 import { BasicActionsList } from "./app/components/BasicActions"
 import { ConfigModal } from "./app/components/ConfigModal"
 import { CurrentHero } from "./app/components/CurrentHero"
 import { StatsTracking } from "./app/components/StatsTracking"
+import { useAppSelector } from "./app/hooks"
+
 
 const App = () => {
+  const hero = useAppSelector(state => state.hero.hero)
+
   return (
     <StyledEngineProvider injectFirst>
       <Container maxWidth={"xl"} disableGutters={true}>
+        <Typography variant="h4" align="center">
+          {hero.name}
+        </Typography>
         <ConfigModal />
         <Grid
           container
