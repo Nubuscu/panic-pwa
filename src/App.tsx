c996710aefc3a1ccb59048d8dc02bae404c218bb
Tyler Kennedy <zippyjr1@gmail.com>
2024-09-07 14:25:00 +1200

    Revert "Move v1 to /v1/ route, add redirect"

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,7 @@ const App = () => {
         </Grid>
         <footer>
           <Typography variant="caption">
-            Found a bug? Report it on{" "}
-            <a href="https://github.com/Nubuscu/panic-pwa/issues">Github</a>
+            Found a bug? Report it on <a href="https://github.com/Nubuscu/panic-pwa/issues">Github</a>
           </Typography>
         </footer>
       </Container>
